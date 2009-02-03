5f018b5a875511b552eb30e7a96afffce9fcde5e
unknown <Administrator@.(none)>
2009-02-02 20:11:13 -0500

    clojure

--- emacs-contrib.el
+++ emacs-contrib.el
@@ -13,3 +13,10 @@
 (add-to-list 'load-path "~/emacs-contrib/color-theme-6.6.0")
 
 
+;;; clojure-mode 
+;;; http://github.com/jochu/clojure-mode/tree/master
+
+(add-to-list 'load-path "~/emacs-contrib/clojure-mode")
+
+(autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
