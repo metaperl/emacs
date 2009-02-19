3b61e1b941a58dc16d121a986fa81ff9b3b38089
U-cybertech-212\Terrence Brannon <Terrence Brannon@cybertech-212.(none)>
2009-02-19 11:33:44 -0500

    emacs-contrib.el

--- emacs-contrib.el
+++ emacs-contrib.el
@@ -27,7 +27,7 @@
 (add-to-list 'load-path "~/emacs-contrib/cperl-mode")
 
 
-=======
+
 ;;; asciidoc
 
 (add-to-list 'load-path "~/emacs-contrib/asciidoc-el")
@@ -46,14 +46,20 @@
 ;;; clojure-mode 
 ;;; http://github.com/technomancy/clojure-mode/tree/master
 
-(add-to-list 'load-path "c:/tmp/c/technomancy-clojure-mode")
-(require 'clojure-mode)
+(if (eq system-type 'windows-nt)
+    (progn
 
-(setq clojure-src-root "c:/tmp/c")
-(clojure-slime-config)
+      (add-to-list 'load-path "c:/tmp/c/technomancy-clojure-mode")
+      (require 'clojure-mode)
 
-(autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+      (setq clojure-src-root "c:/tmp/c")
+      (clojure-slime-config)
+
+      (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
+      (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+      )
+  )
+
 
 
 
