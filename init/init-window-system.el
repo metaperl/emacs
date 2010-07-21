d5f9929efddece643ac94b18a289fb5ade35e756
metaperl <metaperl@andLinux.(none)>
2010-07-21 09:41:50 -0400

    windowing

--- init/init-window-system.el
+++ init/init-window-system.el
@@ -11,4 +11,20 @@
 
 (if (eq window-system 'x) (setq x-select-enable-clipboard t))
 
+;;; http://groups.google.com/group/gnu.emacs.help/browse_thread/thread/01b7d4153acf6336#
+(defun MakeSixWindows ()
+  (interactive)
+  (delete-other-windows)
+  (split-window-vertically)
+  (setq windowColumns
+    (let ((edges (window-edges))) (- (nth 2 edges) (nth 0 edges))) )
+  (split-window-horizontally (round (* windowColumns 0.66)))
+  (split-window-horizontally)
+  (previous-multiframe-window)
+  (split-window-horizontally (round (* windowColumns 0.66)))
+  (split-window-horizontally)
+)
+
+(global-set-key (kbd "C-x 6") 'MakeSixWindows)
+
 (provide 'init-window-system)
