6d3529d3a151d12aa34e8fc9ee5b3fd894f47ebb
metaperl <metaperl@gmail.com>
2013-03-14 09:53:13 -0400

    com

--- init/init-workgroups.el
+++ init/init-workgroups.el
@@ -1,11 +1,14 @@
-(setq wg-file "~/emacs/etc/workgroups/workgroups.data")
+(setq workgroups-data "~/.emacs.d/workgroups.data")
+
+(setq wg-file workgroups-data)
 
 (add-to-list 'load-path "~/emacs/pkg/workgroups.el/")
 (require 'workgroups)
 
 (workgroups-mode 1)
 
-(wg-load "~/emacs/etc/workgroups/workgroups.data")
+(wg-load workgroups-data)
 
-
+; (define-key global-map [(control ?9)] (make-keymap 'control-9-keymap))
+; (global-set-key [(control ?9) (control ?b)] (lambda () (interactive) (find-file "~/.bash_profile")))
 (provide 'init-workgroups)
