d4cc4540b3cd550f6f03b9a9277200cbd6ed7752
metaperl <metaperl@gmail.com>
2014-01-23 09:13:50 -0500

    add

---
+++ init/init-custom-packages.el
@@ -0,0 +1,18 @@
+
+(add-to-list 'load-path "~/emacs/pkg")
+
+
+(setq package-archives '(
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(package-initialize)
+
+;(require 'init-load-packages)
+
+(require 'init-helm)
+(require 'init-flx-ido)
+(require 'init-prodigy)
+
+(require 'init-org)
+
+(provide 'init-custom-packages)
