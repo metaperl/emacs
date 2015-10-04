56648185fce14acb35dd43058bffd800fbcad8f3
Terrence Brannon <metaperl@gmail.com>
2015-10-04 09:46:25 -0400

    something

--- init/init-font.el
+++ init/init-font.el
@@ -25,10 +25,6 @@
 
 
 
-
-(add-to-list 'initial-frame-alist '(font . my-font))
-(add-to-list 'default-frame-alist '(font . my-font))
-
 ; (text-scale-normal-size)
 (text-scale-increase 1)  ; 2 steps larger
 (text-scale-decrease 1)  ; 2 steps larger
